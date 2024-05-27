a3bd1cc82b1138ec64ba701b0f7f6f08fd6b4277
03Arya <142574149+03Arya@users.noreply.github.com>
2024-05-27 13:46:24 +0200

    updated more styling

--- src/pages/playlists/[id].js
+++ src/pages/playlists/[id].js
@@ -16,12 +16,13 @@ export default function Playlist() {
 
     return (
         <>
-            <main className="mx-auto max-w-lg">
+            <main className="dark:bg-black transition duration-500 mx-auto max-w-lg">
                 <Header />
                 <div className=''>
-                    <img className='mx-auto' src={playlist.images[0].url} alt={playlist.name} width="200px" height="200px" />
-                    <h1 className='text-center font-bold text-xl max-w-64 py-4 mx-auto'>{playlist.name}</h1>
-                    <ul className='mx-auto grid max-w-96 gap-8'>
+                    <h1 className='font-bold text-3xl max-w-96 mx-auto py-4'>Playlists</h1>
+                    <img className='mx-auto pt-5' src={playlist.images[0].url} alt={playlist.name} width="200px" height="200px" />
+                    <h2 className='text-center font-bold text-xl max-w-64 py-4 mx-auto'>{playlist.name}</h2>
+                    <ul className='mx-auto grid max-w-96 gap-8 pt-5'>
                         {tracks.map((trackItem, index) => (
                             <li className='grid' key={index}>
                                 <div className='grid'>
@@ -34,13 +35,15 @@ export default function Playlist() {
                                     ))}
                                 </div>
                                 {isPremium ? (
-                                    <iframe src={`https://open.spotify.com/embed/track/${trackItem.track.id}`} width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                                    <iframe src={`https://open.spotify.com/embed/track/${trackItem.track.id}`} height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                                 ) : (
                                     <audio className='h-10' controls src={trackItem.track.preview_url}>Your browser does not support the audio element.</audio>
                                 )}
                             </li>
                         ))}
                     </ul>
+                    <a className="listenAllButton text-pink-700 rounded-full py-3 text-center grid font-bold w-full my-20 max-w-80 mx-auto">LISTEN ALL</a>
+
                 </div>
                 <Footer />
             </main>
