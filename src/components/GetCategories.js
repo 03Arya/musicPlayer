995576324a3f85ffd38015fee4b3d02b57dac4fa
03Arya <142574149+03Arya@users.noreply.github.com>
2024-05-27 09:20:34 +0200

    updated categorylist styling

--- src/components/GetCategories.js
+++ src/components/GetCategories.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { EllipsisHorizontal } from "react-ionicons";
+import { ChevronForwardOutline } from "react-ionicons"
 import Link from "next/link";
 
 export default function GetCategories() {
@@ -43,11 +44,12 @@ export default function GetCategories() {
             <section className="grid gap-2 px-2">
                 {categories.map(category => (
                     <div className="max-w-80 mx-auto w-80" key={category.id}>
-                        <p className="max-w-80 mx-auto bg-pink-600 rounded-md text-white py-6 pl-6 grid grid-cols-2 grid-rows-1" onClick={() => fetchPlaylists(category.id)}>{category.name}<EllipsisHorizontal color="white" className="mx-auto pl-10" /></p>
+                        <p className="max-w-80 mx-auto bg-pink-600 rounded-md font-bold text-white py-6 pl-6 grid grid-cols-2 grid-rows-1" onClick={() => fetchPlaylists(category.id)}>{category.name}<EllipsisHorizontal color="white" className="mx-auto pl-10" /></p>
                         <div className="px-6 gap-2 grid">
                             {selectedCategory === category.id && playlists.map(playlist => (
-                                <div key={playlist.id}>
-                                    <Link href={`/playlists/${playlist.id}`}>{playlist.name}</Link>
+                                <div className="grid grid-cols-2 my-2" key={playlist.id}>
+                                    <Link className="w-64 text-sm" href={`/playlists/${playlist.id}`}>{playlist.name}</Link>
+                                    <ChevronForwardOutline className="justify-end grid" />
                                 </div>
                             ))}
                         </div>
