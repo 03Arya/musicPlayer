d27b30588c9de70bd85b85543c6d690d41612c60
03Arya <142574149+03Arya@users.noreply.github.com>
2024-06-04 09:46:41 +0200

    added more to the albums page

--- src/pages/albums.js
+++ src/pages/albums.js
@@ -1,5 +1,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Albums() {
@@ -18,6 +19,8 @@ export default function Albums() {
             setData(data);
             if (data.albums) {
                 setAlbums(data.albums.items);
+                console.log(data);
+
             }
         }
         fetchData();
@@ -25,19 +28,43 @@ export default function Albums() {
 
 
     return (
-        <main className="dark:bg-purple transition duration-500 mx-auto max-w-lg">
+        <main className="dark:bg-purple transition duration-500 mx-auto max-w-lg h-screen">
             <Header />
-            <div>
-                <div className="max-w-80 mx-auto py-4">
-                    <h1 className="max-w-xs mx-auto font-bold text-3xl Gradient">All Albums</h1>
-                </div>
-                {albums.map(album => (
-                    <div className="" key={album.id}>
-                        <img className="rounded-md mx-auto grid justify-center left-0 right-0" src={album.images[0].url} alt={album.name} width="130px" height="130px" loading="lazy" />
-
-                    </div>
-                ))}
+            <div className="max-w-lg mx-5 py-4">
+                <h1 className="max-w-xs mx-auto font-bold text-3xl Gradient">All Albums</h1>
             </div>
+            <section>
+                <div className="max-w-lg mx-5 grid grid-cols-2 py-6">
+                    <p className="font-bold">Featured Albums</p>
+                    <Link href="" className="text-end text-pink-600">View All</Link>
+                </div>
+                <div className="flex gap-4 flex-row overflow-x-auto no-scrollbar mx-5 pb-6">
+                    {albums.map(album => (
+                        <div className="" key={album.id}>
+                            <img className="rounded-md w-40 h-40 max-w-40 mx-auto grid justify-center left-0 right-0" src={album.images[0].url} alt={album.name} loading="lazy" />
+
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <section>
+                <div className="max-w-lg mx-5 grid grid-cols-2 py-6">
+                    <p className="font-bold">Best Sellers</p>
+                    <Link href="" className="text-end text-pink-600">View All</Link>
+                </div>
+                <div className="flex gap-4 flex-col overflow-x-auto no-scrollbar mx-5 pb-6">
+                    {albums.map(album => (
+                        <div className="grid grid-cols-3" key={album.id}>
+                            <img className="rounded-md w-20 h-20 max-w-40 grid justify-start" src={album.images[0].url} alt={album.name} loading="lazy" />
+                            <div className="w-40">
+                                <p className="font-bold text-sm">{album.artists[0].name}</p>
+                                <p className="text-xs pt-4">{album.name}</p>
+                            </div>
+                            <p className="text-end text-xs">{album.total_tracks} Songs</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
             <Footer />
         </main>
 
